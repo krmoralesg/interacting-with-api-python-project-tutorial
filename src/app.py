80687938aaeddbde126679148128e0272635d933
krmoralesg <51345919+krmoralesg@users.noreply.github.com>
2023-10-16 19:06:51 +0000

    hello

--- src/app.py
+++ src/app.py
@@ -1,15 +1,30 @@
-import os
-from sqlalchemy import create_engine
-import pandas as pd
 from dotenv import load_dotenv
-
-# load the .env file variables
 load_dotenv()
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+import pandas as pd
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+# Initialize Spotify client
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+def get_artist_data(artist_id):
+    artist = sp.artist(artist_id)
+    return artist
+
+def get_artist_top_tracks(artist_id):
+    top_tracks = sp.artist_top_tracks(artist_id)
+    return top_tracks['tracks']
+
+# Example usage
+artistId = '0L8ExT028jH3ddEcZwqJJ5'  # Replace with the artist's Spotify ID
+artistData = get_artist_data(artistId)
+top_tracks = get_artist_top_tracks(artistId)
+df = pd.DataFrame(top_tracks)
+
+# Display the DataFrame
+print(df[['duration_ms', 'name']])
